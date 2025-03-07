63280213dcfc66f2726a5bcfa476cc866e16f03a
Aryan Gupta <maraanujbhai123@gmail.com>
2025-03-07 18:18:18 +0530

    deploy changes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
+// require('dotenv').config();
 const workoutRoutes = require('./routes/workouts');
 
 // express app
 const app = express();
-
+const MONGO_URI="mongodb+srv://merntester:1234@cluster0.7rn9h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const PORT = 4000
 // middle ware
 app.use(express.json()); // if req has some data, this adds it to req. object.
 app.use((req, res, next)=>{
@@ -22,11 +23,11 @@ app.use('/api/workouts', workoutRoutes); // use the routes written in ./routes/w
 // the /api/workouts here and the one written in workouts.js file are going to be relative to its folder...i.e. the paths are concatenated.
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI) // asyncronous
+mongoose.connect(MONGO_URI) // asyncronous
     .then(() => {
         // listening for requests
-        app.listen(process.env.PORT, () => {
-            console.log(`connected to DB and Listening on port ${process.env.PORT}...`);
+        app.listen(PORT, () => {
+            console.log(`connected to DB and Listening on port ${PORT}...`);
         });
     })
     .catch((err) => {
